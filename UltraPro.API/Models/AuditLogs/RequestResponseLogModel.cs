1cd911d2594c892a55b7807e0a22b4729f0ea19f
Mahmud Koli <mahmudkolirpi@gmail.com>
2021-05-04 00:50:17 +0600

    Log model update

---
+++ UltraPro.API/Models/AuditLogs/RequestResponseLogModel.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UltraPro.API.Mappings;
+using UltraPro.Entities;
+
+namespace UltraPro.API.Models.Logs
+{
+    public class RequestResponseLogModel : IMapFrom<RequestResponseLog>
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Scheme { get; set; }
+        public string Host { get; set; }
+        public string Path { get; set; }
+        public string QueryString { get; set; }
+        public string Method { get; set; }
+        public string ContentType { get; set; }
+        public string Payload { get; set; }
+        public string Response { get; set; }
+        public int ResponseCode { get; set; }
+        public DateTime RequestedOn { get; set; }
+        public DateTime RespondedOn { get; set; }
+        public bool IsSuccessStatusCode { get; set; }
+
+        public RequestResponseLogModel()
+        {
+
+        }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<RequestResponseLog, RequestResponseLogModel>();
+        }
+    }
+}
